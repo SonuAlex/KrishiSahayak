11f75b86df8b22b8f95f6485d77eb999c55df014
SonuAlex <instructable01@gmail.com>
2024-03-09 15:36:02 +0530

    Update crop.js
    Changed the capital requirement

--- crop.js
+++ crop.js
@@ -1,15 +1,34 @@
 const crop_search = document.querySelector('.stateSelector button');
 const crop_searchInput = document.querySelector('.stateSelector input');
 
+function capitalizeEveryWord(str) {
+    let words = str.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        const firstLetter = words[i].charAt(0);
+        const restOfWord = words[i].slice(1);
+
+        if (firstLetter === firstLetter.toUpperCase()) {
+            words[i] = firstLetter + restOfWord.toLowerCase();
+        } else {
+            words[i] = firstLetter.toUpperCase() + restOfWord.toLowerCase();
+        }
+    }
+
+    return words.join(' ');
+}
+
+
 function performCropSearch() {
     const APIKey = '579b464db66ec23bdd000001d58e4dcb922348a949989f902c29e4ce';
-    const state = document.querySelector('.stateSelector input').value;
+    const state1 = document.querySelector('.stateSelector input').value;
 
-    if(state == ''){
+    if(state1 == ''){
         console.log('Empty Input');
         return;
     }
 
+    const state = capitalizeEveryWord(state1);
     cropURL = `https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=${APIKey}&format=json&filters%5Bstate%5D=${state}`
     fetch(cropURL).then(response => response.json()).then(data => {
         const record = data.records;
